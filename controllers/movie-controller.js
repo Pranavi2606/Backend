cfb95a313a26712feae34127dde3c0853ae22edd
Pranavi2606 <94110080+Pranavi2606@users.noreply.github.com>
2023-11-20 20:35:28 +0530

    Update movie-controller.js

--- controllers/movie-controller.js
+++ controllers/movie-controller.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-import Movie from "../models/Movie";
-import Admin from "../models/Admin";
+import Movie from "../models/Movie.js";
+import Admin from "../models/Admin.js";
 
 export const addMovie = async (req,res,next) => {
     const extractedToken =req.headers.authorization.split(" ")[1];
